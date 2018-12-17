b59e4c1397c35e83c7cbe017c54993a5eff07382
parkminho <minho4377@daum.net>
2018-12-18 02:35:35 +0900

    #4-9 LoginScreen part Three(글작성이 동시에 됨 니콜샘거 비교해서 찾아내고 고치기 presenter.js만 고치면됨)

--- screens/LogInScreen/presenter.js
+++ screens/LogInScreen/presenter.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { 
     View, 
     Text, 
@@ -7,7 +8,8 @@ import {
     Dimensions, 
     TouchableOpacity,
     TextInput,
-    StatusBar
+    StatusBar,
+    ActivityIndicator
 } from "react-native";
 
 import {Ionicons} from "@expo/vector-icons";
@@ -29,6 +31,8 @@ const LogInScreen = props => (
                 style={styles.textInput} 
                 autoCapitalize={"none"}
                 autoCorrect={false}
+                value={props.username}
+                onChangeText ={props.changeUsername}
                 
                 />
 
@@ -36,10 +40,18 @@ const LogInScreen = props => (
                 style={styles.textInput} 
                 autoCapitalize={"none"}
                 secureTextEntry={true}
+                value={props.username}  
+                onChangeText = {props.changePassword}
+                returnKeyType={"send"}
+                onSubmitEditing={props.submit}
                 />
-        <TouchableOpacity style={styles.touchable}>
+        <TouchableOpacity style={styles.touchable} onPressOut={props.submit}>
             <View style={styles.button}>
-          <Text style={styles.btnText}>Log In</Text>
+             {props.isSubmitting ? (
+              <ActivityIndicator size="small" color="white"/>
+              ):(
+              <Text style={styles.btnText}>Log In</Text>
+              )}
             </View>
         </TouchableOpacity>
         <TouchableOpacity style={styles.fbContainer}>
@@ -53,6 +65,16 @@ const LogInScreen = props => (
     </View>
   </View>
 );
+
+
+LogInScreen.propTypes = {
+  isSubmitting: PropTypes.bool.isRequired,
+  username: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired,
+  changeUsername: PropTypes.func.isRequired,
+  changePassword: PropTypes.func.isRequired,
+  submit: PropTypes.func.isRequired
+};
 
 
 const styles = StyleSheet.create({
