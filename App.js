6ce6604cd65f79a7da41dac6da57c36109d1d6bc
parkminho <minho4377@daum.net>
2018-12-17 19:36:55 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { AppLoading, Asset, Font } from "expo";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
+import { StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/es/integration/react";
+import configureStore from "./redux/configureStore";
+const { persistor, store } = configureStore();
+import AppContainer from "./components/AppContainer";
+
+class App extends React.Component {
+  state = {
+    isLoadingComplete: false
+  };
+  render() {
+    const { isLoadingComplete } = this.state;
+    if (!isLoadingComplete) {
+      return (
+        <AppLoading
+          startAsync={this._loadAssetsAsync}
+          onError={this._handleLoadingError}
+          onFinish={this._handleFinishLoading}
+        />
+      );
+    }
+    return (
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <AppContainer />
+        </PersistGate>
+      </Provider>
+    );
+  }
+  _loadAssetsAsync = async () => {
+    return Promise.all([
+      Asset.loadAsync([
+        require("./assets/logo.png"),
+        require("./assets/logo-white.png"),
+        require("./assets/noPhoto.jpg"),
+        require("./assets/photoPlaceholder.png")
+      ]),
+      Font.loadAsync({
+        ...Ionicons.font,
+        ...MaterialIcons.font
+      })
+    ]);
+  };
+  _handleLoadingError = error => {
+    console.error(error);
+  };
+  _handleFinishLoading = async () => {
+    this.setState({
+      isLoadingComplete: true
+    });
+  };
+}
+
+export default App;
